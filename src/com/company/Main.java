1ba251cee19ac85b0786c5450715faef16f7f611
Apafios <andreiilevencov@gmail.com>
2022-04-13 15:20:02 +0300

    2/3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Box a = new Box(25,17,44,Material.WOOD);
         ColorBox b = new ColorBox(1,738,141,Material.METAL,null);
+        Warehouse warehouse = new Warehouse();
+//        Object[] array = new Object[];
+
+
 
         System.out.println("Ящик мой - a : " + a);
         System.out.println("Ящик мой - b : " + b);
--- src/com/company/Warehouse.java
+++ src/com/company/Warehouse.java
@@ -1,5 +1,6 @@
 package com.company;
 
 public class Warehouse {
+    protected  int capacity;
 
 }
